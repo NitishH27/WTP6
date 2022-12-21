96ced568dd8fbceadbce5345c2cdf08177cce743
NitishH27 <100903164+NitishH27@users.noreply.github.com>
2022-12-21 07:59:16 +0000

    p6a trials

---
+++ person.js
@@ -0,0 +1,12 @@
+let http = require('http');
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    var Person={Fname:`Rina`,Lname:`Mehta`,age:35, full_name : function() {
+      return this.Fname + " " + this.Lname;}};-
+
+    res.write(JSON.stringify("sammy"));
+    res.end(`{"message": "This is a JSON response"}`);
+};
+http.createServer(requestListener).listen(9000);
